f6293fa9d8086a0287920bd36d8433b5d057717d
Christian Soriani <57401427+chris190698@users.noreply.github.com>
2022-06-16 20:56:42 +0200

    Delete client directory

--- client/functions/getReport.php
+++
@@ -1,8 +0,0 @@
-<?php
-$self = curl($baseUrl . 'api/users/' . $_COOKIE['token'] . '/getSelf');
-
-require dirname(__DIR__, 1) . '/layouts/pdfReport.php';
-
-$html2pdf->setDefaultFont('helvetica');
-$html2pdf->writeHTML($htmlReportPage);
-$html2pdf->output($tmpDir . '/report_' . $request_date_underscore .'.pdf', 'F');
